eddc8c11492610858a844e39de9f566b18975288
Bryan <bsg262@gmail.com>
2022-02-04 18:35:17 -0500

    database changes

--- possibility_app/models.py
+++ possibility_app/models.py
@@ -3,11 +3,8 @@ from . import db
 
 class Subject(db.Model):
     __tablename__ = 'subjects'
-    id = db.Column(db.Integer, primary_key=True)
-    subID = db.Column(db.Integer, index=True, unique=False)
-    prolific_id = db.Column(db.String(64), unique=True)
+    prolific_id = db.Column(db.String(64), unique=True, primary_key=True, index=True)
     trustee_id = db.Column(db.Integer)
-    trial_order = db.Column(db.Integer)
     bonus = db.Column(db.Float)
     exp_feedback = db.Column(db.VARCHAR(300))
     trials = db.relationship('Trial', backref='subject', lazy='dynamic', cascade="all, delete-orphan")
@@ -19,16 +16,17 @@ class Subject(db.Model):
 class Trial(db.Model):
     __tablename__ = 'trials'
     id = db.Column(db.Integer, primary_key=True)
-    trl = db.Column(db.String(64), unique=False)
-    p1_pic = db.Column(db.String(64))
+    trl = db.Column(db.Integer, unique=False, index=True)
+    p1_pic = db.Column(db.Integer)
     inv = db.Column(db.Integer)
     mult = db.Column(db.Integer)
     pred = db.Column(db.Integer)
     ret = db.Column(db.Integer)
+    probe = db.Column(db.Boolean)
     reason = db.Column(db.VARCHAR(200))
     reason_start = db.Column(db.Integer) # time to start writing
     reason_rt = db.Column(db.Integer) # time to submit response
-    subject_id = db.Column(db.Integer, db.ForeignKey('subjects.id'))
+    prolific_id = db.Column(db.String, db.ForeignKey('subjects.prolific_id'))
 
     def __repr__(self):
         return '<Trial %r>' % self.trl
