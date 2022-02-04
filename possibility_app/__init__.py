34fa7e971887ea177f21d134b1c56a91592bbb76
Bryan <bsg262@gmail.com>
2022-02-04 18:30:23 -0500

    remove unnecessary url rule

--- possibility_app/__init__.py
+++ possibility_app/__init__.py
@@ -3,16 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_bootstrap import Bootstrap
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dfkjfsdlf'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///hmtg_possib_DB.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.add_url_rule('/static', 'static/')
+
 
 db = SQLAlchemy(app)
 cors = CORS(app)
 bootstrap = Bootstrap(app)
+
 
 from . import views
 if __name__ == '__main__':
